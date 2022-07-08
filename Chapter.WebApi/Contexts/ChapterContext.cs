3fd43770e78a258395a64bba07893d9aac200056
Ismael <ismael.dev96@gmail.com>
2022-07-08 09:50:27 -0300

    primeira atualização

---
+++ Chapter.WebApi/Contexts/ChapterContext.cs
@@ -0,0 +1,24 @@
+﻿using Chapter.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.WebApi.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext>options) : base(options){}
+
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-7S1TGCM\\SQLEXPRESS1; initial catalog = Chapter; User Id=sa; password=1234");
+            }
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
